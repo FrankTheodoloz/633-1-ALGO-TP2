4b24f9e920dfca418180407be8eed373240f7b62
Frank Théodoloz <frank@16d.ch>
2019-11-03 20:22:36 +0100

    Ajout paramètres, exception IndexOutOfBounds et temps d'execution

--- src/Main.java
+++ src/Main.java
@@ -11,16 +11,21 @@ import java.util.NoSuchElementException;
 public class Main {
 
     // paramètre pour afficher le résultat
-    private static boolean printResults = true;
+    private static boolean printResults = false;
+    private static int nombre_lignes = 16; // nombre d'éléments; -1 pour tout
 
     public static void main(String[] args) {
 
         try {
-//            useMyArrayList("insertion", "irishtimes-date-text.csv", 1000);
-//            useMyArrayList("fusion", "irishtimes-date-text.csv", 100);
-//            useMyLinkedList("insertion", "irishtimes-date-text.csv", 1000);
-            useMyLinkedList("fusion", "irishtimes-date-text.csv", 1000);
 
+            useMyArrayList("insertion", "irishtimes-date-text.csv", nombre_lignes); // 370 ms pour 2000 éléments
+            useMyLinkedList("insertion", "irishtimes-date-text.csv", nombre_lignes); // 13731ms pour 2000 éléments
+//            useMyArrayList("fusion", "irishtimes-date-text.csv", nombre_lignes); // IndexOutOfBoundsException
+//            useMyLinkedList("fusion", "irishtimes-date-text.csv", nombre_lignes); // Doubles
+
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Erreur : l'index de recherche est hors limite (voir pile d'execution) :");
+            e.printStackTrace();
         } catch (FileNotFoundException e) {
             System.out.println("Erreur : le fichier spécifié est introuvable");
         } catch (ParseException | NoSuchElementException e) {
@@ -33,9 +38,11 @@ public class Main {
 
     }
 
-    private static void useMyLinkedList(String mode, String filename, int number_lines) throws FileNotFoundException, ParseException, InvalidParameterException {
+    private static void useMyArrayList(String mode, String filename, int number_lines)
+            throws FileNotFoundException, ParseException, InvalidParameterException, IndexOutOfBoundsException {
+        long startTime = System.nanoTime();
         FileToStr fileToStr = new FileToStr();
-        MyCollection list = new MyLinkedList();
+        MyCollection list = new MyArrayList();
 
         if (number_lines == -1) {
             list = fileToStr.loadIntoArrayList(filename);
@@ -45,11 +52,16 @@ public class Main {
         }
 
         startSort(mode, list);
+        long elapsedTime = System.nanoTime() - startTime;
+        System.out.print("Temps d'exécution pour " + (number_lines == -1 ? "tous les" : number_lines) + " éléments");
+        System.out.println(" avec MyArrayList (tri par " + mode + ") : " + elapsedTime / 1000000 + " ms.");
     }
 
-    private static void useMyArrayList(String mode, String filename, int number_lines) throws FileNotFoundException, ParseException, InvalidParameterException {
+    private static void useMyLinkedList(String mode, String filename, int number_lines)
+            throws FileNotFoundException, ParseException, InvalidParameterException, IndexOutOfBoundsException {
+        long startTime = System.nanoTime();
         FileToStr fileToStr = new FileToStr();
-        MyCollection list = new MyArrayList();
+        MyCollection list = new MyLinkedList();
 
         if (number_lines == -1) {
             list = fileToStr.loadIntoDoublyLinkedList(filename);
@@ -59,6 +71,9 @@ public class Main {
         }
 
         startSort(mode, list);
+        long elapsedTime = System.nanoTime() - startTime;
+        System.out.print("Temps d'exécution pour " + (number_lines == -1 ? "tous les" : number_lines) + " éléments");
+        System.out.println(" avec MyDoublyLinkedList (tri par " + mode + ") : " + elapsedTime / 1000000 + " ms.");
     }
 
     private static void startSort(String mode, MyCollection list) throws InvalidParameterException {
